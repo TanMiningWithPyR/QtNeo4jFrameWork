e013a6da1ca9f6a2978c0bd9e6fa61265e3fe37f
Affluence Tan <fuyou_tan@sina.cn>
2019-10-08 22:09:34 +0800

    pd to dict

--- Config/ConfigRead.py
+++ Config/ConfigRead.py
@@ -5,31 +5,28 @@ Created on Mon Oct  7 09:08:21 2019
 @author: Fuyou Tan
 """
 
-import numpy as np
+#import numpy as np
 import pandas as pd
 
+class ClassConfig():
+    def __init__(self):
+        self.class_name = pd.read_excel("config.xlsx",'ClassName').set_index('class_id')
+        self.class_structure = pd.read_excel("config.xlsx",'ClassStructure').set_index(['class_id','attribute_id'])
+    
+    def getClassInfo(self, class_id):
+        return self.class_name.loc[class_id].to_dict()
+        
+    def getClassAttributes(self, class_id):
+        return self.class_structure.loc[class_id].to_dict(orient='index')
+        
+    def getAttributeInfo(self, class_id, attribute_id):
+        return self.class_structure.loc[class_id].loc[attribute_id].to_dict()
+        
 if __name__=='__main__':  
-    class_name = pd.read_excel("config.xlsx",'ClassName').set_index('class_id')
-    tblTableName = tblTableName.set_index("id")
-    tbl_graph.create_nodes(tblTableName, 'Table')
-    tbl_graph.graph.run("CREATE CONSTRAINT ON (c_id:Table) ASSERT c_id.id IS UNIQUE")
-    
-    tblTableStruction = pd.read_csv("Neo4j_files\\tblTableStruction_utf.csv",
-                                    dtype = {'field_width':np.float,
-                                             'field_precision':np.float,
-                                             'index':np.str})
-    tblTableStruction = tblTableStruction.set_index("id")
-    tbl_graph.create_nodes(tblTableStruction, 'Field')
-    tbl_graph.graph.run("CREATE CONSTRAINT ON (c_id:Field) ASSERT c_id.id IS UNIQUE")
-    
-    tbl_graph.create_relations(from_node_label='Table',
-                               from_node_property='id',
-                               to_node_label='Field',
-                               to_node_property='table_id',
-                               relation_type='Have_Field')
-    
-    tbl_graph.create_relations(from_node_label="Field",
-                               from_node_property="referance",
-                               to_node_label="Field",
-                               to_node_property="id",
-                               relation_type='Referance')
+    class_config = ClassConfig()
+    cls_ent_aaa = class_config.getClassInfo('cls_ent_aaa')
+    print(cls_ent_aaa)
+    attr_of_cls_ent_aaa = class_config.getClassAttributes('cls_ent_aaa')
+    print(attr_of_cls_ent_aaa)
+    attr_info = class_config.getAttributeInfo('cls_ent_aaa','attr_aaa')
+    print(attr_info)
